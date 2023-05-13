40d3ed5fa810d094bd3d3043eb313e6a76c24c39
GoldenMEmre <goldenpie60@gmail.com>
2023-05-13 15:42:16 +0300

    10.gun sonu

---
+++ src/test/java/day_10_actionsClass_Faker_FileTestleri/C05_FileDownloadTest.java
@@ -0,0 +1,45 @@
+package day_10_actionsClass_Faker_FileTestleri;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C05_FileDownloadTest extends TestBase{
+
+    @Test
+    public void test01() throws InterruptedException {
+
+        //2. https://the-internet.herokuapp.com/download adresine gidelim.
+        driver.get("https://the-internet.herokuapp.com/download");
+        //3. n.png dosyasını indirelim
+        WebElement fotoElementi= driver.findElement(By.linkText("n.png"));
+        fotoElementi.click();
+        Thread.sleep(3000);
+        //4. Dosyanın başarıyla indirilip indirilmediğini test edelim
+
+        // Dosyayi İndirilenler klasorune indirecek
+
+        //String dosyaYolu="C:\Users\hasba\Downloads\n.png";
+
+
+        //Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+        // herkesin bilgisayarinda farkli bir dosya hiyerarsisi oldugundan
+        // herkesin dosya yolu birbirinden farkli olur
+        // Dosya yolunu dinamik hale getirmek icin Java'dan yardim alacagiz
+        //C:\Users\emre.cigit\Downloads\foto.png"
+        //"C:\Users\Berke\Downloads\foto.png"
+
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("user.home"));
+
+    }
+
+
+
+}
---
+++ src/test/java/day_10_actionsClass_Faker_FileTestleri/C08_Waits.java
@@ -0,0 +1,12 @@
+package day_10_actionsClass_Faker_FileTestleri;
+
+import org.junit.Test;
+
+public class C08_Waits {
+
+    @Test
+    public void test01(){
+
+
+    }
+}
